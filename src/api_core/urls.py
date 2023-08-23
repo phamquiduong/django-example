bdb33fed9f042758c4765daae096ae3584569055
Pham Qui Duong <phamquiduong@outlook.com>
2023-08-24 03:25:48 +0700

    Update swagger code smell

--- src/api_core/urls.py
+++ src/api_core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 
-from .views import schema_view
+from api_core.views import SchemaView
 
 urlpatterns = [
     path('', include('api_auth.urls')),
-    path('docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema_json'),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
-    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+    path('docs<format>/', SchemaView.without_ui(cache_timeout=0), name='schema_json'),
+    path('docs/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
+    path('redocs/', SchemaView.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
 ]
--- src/api_core/views.py
+++ src/api_core/views.py
@@ -2,7 +2,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-schema_view = get_schema_view(
+SchemaView = get_schema_view(
     openapi.Info(
         title="Django RestFramework",
         default_version='v1',
