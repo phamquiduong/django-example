103a6bf6b148381e027428fc54301323e5d02ffd
Pham Qui Duong <phamquiduong@outlook.com>
2023-08-24 03:30:42 +0700

    Update swagger url

--- src/api_core/urls.py
+++ src/api_core/urls.py
@@ -4,7 +4,7 @@ from api_core.views import SchemaView
 
 urlpatterns = [
     path('', include('api_auth.urls')),
-    path('docs<format>/', SchemaView.without_ui(cache_timeout=0), name='schema_json'),
-    path('docs/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
-    path('redocs/', SchemaView.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+    path('', SchemaView.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
+    path('doc<format>/', SchemaView.without_ui(cache_timeout=0), name='schema_json'),
+    path('re-doc/', SchemaView.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
 ]
